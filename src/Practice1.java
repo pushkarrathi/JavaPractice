4805c0432b99ac4077c49c65e9a2943e49aef512
pushkarrathi <pushkarrathi1001@gmail.com>
2025-08-14 23:32:43 +0530

    Area of Circle

--- src/Practice1.java
+++ src/Practice1.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Practice1 {
+public static double AreaCircle(){
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter radius of circle");
+	double r = sc.nextDouble();
+	return Math.PI*Math.pow(r,2);
+}
 public static void main(String[] args) {
-	System.out.println("Practice 1");
+	System.out.println(AreaCircle());
 }
 }

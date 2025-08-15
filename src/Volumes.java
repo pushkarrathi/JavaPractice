0d5e05722285fa2afb4b45b88f897946427ce10b
pushkarrathi <pushkarrathi1001@gmail.com>
2025-08-15 23:25:31 +0530

    Volume of prism updated

--- src/Volumes.java
+++ src/Volumes.java
@@ -8,7 +8,15 @@ public static double volumeCone() {
 	double h = sc.nextDouble();
 	return Math.PI*Math.pow(r,2)*h/3;
 }
+public static double volumePrism() {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter base area and height of cone");
+	double ba = sc.nextDouble();
+	double h = sc.nextDouble();
+	return ba*h;
+}
 public static void main(String[] args) {
 	System.out.println(volumeCone());
+	System.out.println(volumePrism());
 }
 }

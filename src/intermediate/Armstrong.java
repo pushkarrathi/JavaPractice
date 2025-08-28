007f00fdf04a7de19d478ac96b191be121be1dfe
pushkarrathi <pushkarrathi1001@gmail.com>
2025-08-29 00:43:35 +0530

    Leap year checker

--- src/intermediate/Armstrong.java
+++ src/intermediate/Armstrong.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Armstrong {
 public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter number to check for armstrong number: ");
 	int n = sc.nextInt();
 	int num = n;
 	int digits = 0;
